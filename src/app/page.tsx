17f8a182a501f046c8ef7124ebc4d077b7a1cf55
asallh <ajaypalsallh@gmail.com>
2024-11-29 14:27:44 -0700

    adding basic scrapping functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,52 @@
-"use client"
+"use client";
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [download, setDownload] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
 
   // Will fetch the page data
-  const getPageData = () =>{
-    fetch("http://localhost:3000/api/getData")
-  }
+  const getPageData = async () => {
+    const res = await fetch("http://localhost:3000/api/getData", {
+      method: "POST",
+      body: JSON.stringify({ url }),
+    })
+    const { download } = await res.json();
+    setDownload(download);
+    console.log("Downloads", download);
+  };
 
   return (
     <div className="bg-background min-h-screen flex flex-col items-center justify-center">
       <h1 className="p-8 text-titleColor text-3xl font-bold">Scrafty</h1>
-      <h2 className="text-titleColor p-8">Enter a URL to download the page as a text file and all image assets from the webpage!</h2>
+      <h2 className="text-titleColor p-8">
+        Enter a URL to download the page as a text file and all image assets
+        from the webpage!
+      </h2>
       <div className="w-full max-w-3xl">
         <Input
           label="Website"
           required
-          width="100%" 
+          width="100%"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
         />
         <div className="flex justify-center mt-8">
-          <Button variant="solid" className="m-4 p-4 bg-buttonColor hover:bg-buttonHover" onClick={getPageData} >Submit</Button>
+          <Button
+            variant="solid"
+            className="m-4 p-4 bg-buttonColor hover:bg-buttonHover"
+            onClick={getPageData}
+          >
+            Submit
+          </Button>
         </div>
+        {download && (
+          <center>
+            <p> This Package has {download} downloads.</p>
+          </center>
+        )}
       </div>
     </div>
   );
--- src/pages/api/getData.tsx
+++ src/pages/api/getData.tsx
@@ -1,9 +1,24 @@
-const getData = async () => {
-    const res = await fetch('https://dailyhive.com/calgary/calgary-flames-game-unusual-time-blue-jackets')
-    const html = await res.text()
+import {JSDOM} from 'jsdom'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-    console.log("html", html)
-    return ""
+const getData = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    // receive the imnput from the body 
+    const body = JSON.parse(req.body)
+    const {url} = body
+    console.log("URL", url)
+    const response = await fetch(`https://www.npmjs.com/package/${url.toLowerCase()}`)
+    const html = await response.text()
+
+    const dom = new JSDOM(html)
+    const document = dom.window.document
+
+    const download = document.querySelector('._9ba9a726')?.textContent
+
+    console.log("Downloads", download)
+
+    // send downloads back to client
+    res.status(200).json({download})
 }
 
 export default getData;
